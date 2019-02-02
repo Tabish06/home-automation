ecc624356879f6e2227617e5dc4f5c85d3a519ff
Tabish M <tabish.maniar@gmail.com>
2019-02-02 03:45:03 -0500

    user session complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if !logged_in?
+      @user = User.new
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new', as: 'login'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
 
   resources :users
 
